c5de365e903519568c5a405ee7094fb587535e23
Baodi Shan <lwshanbd@gmail.com>
2022-12-12 15:22:00 -0500

    modified:   num_devices.cu

--- num_devices.cu
+++ num_devices.cu
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+// Macro for checking errors in CUDA API calls
+#define cudaErrorCheck(call)                                                              \
+do{                                                                                       \
+	cudaError_t cuErr = call;                                                             \
+	if(cudaSuccess != cuErr){                                                             \
+		printf("CUDA Error - %s:%d: '%s'\n", __FILE__, __LINE__, cudaGetErrorString(cuErr));\
+		exit(0);                                                                            \
+	}                                                                                     \
+}while(0)
+
 int main()
 {
     int NumOfDevices;
@@ -11,5 +21,5 @@ int main()
     printf("number of devices is %d\n", NumOfDevices);
 
     return 0;
-    
+
 }
